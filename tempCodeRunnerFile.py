1a392b81567285686494473a6f64f303c8f49e5e
Daniel Chastain <53002994+danielchastain@users.noreply.github.com>
2023-02-15 13:24:47 -0800

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+ys.download("Videos",filename=projectName+fileNumber)
+    # downloadedVideo = "C:/Users/danie/Desktop/Content Farm/Videos/" + projectName+fileNumber+ ".mp4"
+    # urlList.append(downloadedVideo)
+    # print("Stored: ", urlList)
+    # print("counter: ", counter)
+    # counter = counter + 1
+    # if counter == 13:
+    #     break
